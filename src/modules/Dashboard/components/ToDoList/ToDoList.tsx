fc7fbb3c52c6d9f7b2fdf3b9a34678f63438a4ce
Widal <mateuszwidalski@gmail.com>
2022-05-28 18:04:33 +0200

    style: add styles to toDo List

--- src/modules/Dashboard/components/ToDoList/ToDoList.tsx
+++ src/modules/Dashboard/components/ToDoList/ToDoList.tsx
@@ -1,4 +1,4 @@
-import { Button, IconButton, List, ListItem, ListItemText, TextField } from "@mui/material";
+import { Box, Button, Divider, Grid, IconButton, InputBase, List, ListItem, ListItemText, Paper, TextField } from "@mui/material";
 import React, { useState } from "react";
 import styles from "./ToDoList.module.scss";
 import AddIcon from '@mui/icons-material/Add';
@@ -15,20 +15,34 @@ export const ToDoList: React.FC = () => {
 
   return (
     <>
-      <div className={styles.addTodo}>
-        <TextField label="To do note" type="search" value={message} onChange={(event) => setMessage(event.target.value)} />
+      <Grid>
+        <div className={styles.addTodo}>
+          <Paper
+            component="form"
+            sx={{ p: '4px 4px', display: 'flex', alignItems: 'center', width: 500 }}
+          >
 
-        <IconButton size="large" className={styles.addTodoButton} onClick={() => { setMessages((prev) => [...prev, message]); setMessage("") }}>
-          <AddIcon fontSize="inherit" />
-        </IconButton>
-      </div>
-      <List className={styles.toDoList}>
-        {messages.map((message) => (
-          <ListItem className={styles.listElement}>
-            <ListItemText primary={message} />
-          </ListItem>
-        ))}
-      </List>
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Notes"
+              inputProps={{ 'aria-label': 'search google maps' }}
+              value={message} onChange={(event) => setMessage(event.target.value)}
+            />
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton color="primary" sx={{ p: '10px' }} size="large" className={styles.addTodoButton} onClick={() => { setMessages((prev) => [...prev, message]); setMessage("") }}>
+              <AddIcon fontSize="inherit" />
+            </IconButton>
+          </Paper>
+        </div>
+        <List className={styles.toDoList}>
+          {messages.map((message) => (
+            <ListItem className={styles.listElement}>
+              <ListItemText primary={message}
+                sx={{ wordBreak: "break-word" }} />
+            </ListItem>
+          ))}
+        </List>
+      </Grid>
     </>
   );
 };
