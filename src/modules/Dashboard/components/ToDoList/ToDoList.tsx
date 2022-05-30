2b97369ebebcc8f0f1c8b1f525d4287aee238c1b
Mateusz <mateuszwidalski@gmail.com>
2022-05-30 18:09:48 +0200

    feat: add validation to toDo list

--- src/modules/Dashboard/components/ToDoList/ToDoList.tsx
+++ src/modules/Dashboard/components/ToDoList/ToDoList.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, Grid, IconButton, InputBase, List, ListItem, ListItemText, Paper, TextField } from "@mui/material";
+import { Divider, Grid, IconButton, InputBase, List, ListItem, ListItemText, makeStyles, Paper, TextField } from "@mui/material";
 import React, { useState } from "react";
 import styles from "./ToDoList.module.scss";
 import AddIcon from '@mui/icons-material/Add';
@@ -15,25 +15,28 @@ export const ToDoList: React.FC = () => {
 
   return (
     <>
-      <Grid>
+      <Grid alignSelf={"center"} maxWidth={"500px"}>
         <div className={styles.addTodo}>
           <Paper
+            className={styles.inputBox}
             component="form"
             sx={{ p: '4px 4px', display: 'flex', alignItems: 'center', width: 500 }}
           >
-
             <InputBase
               sx={{ ml: 1, flex: 1 }}
               placeholder="Notes"
-              inputProps={{ 'aria-label': 'search google maps' }}
+              inputProps={{ 'aria-label': 'Notes' }}
               value={message} onChange={(event) => setMessage(event.target.value)}
             />
             <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-            <IconButton color="primary" sx={{ p: '10px' }} size="large" className={styles.addTodoButton} onClick={() => { setMessages((prev) => [...prev, message]); setMessage("") }}>
+            <IconButton color="primary" sx={{ p: '10px' }} size="large"
+              className={styles.addTodoButton}
+              onClick={() => { if (message != "" || !/^[ ]*$/.test(message)) { setMessages((prev) => [...prev, message]); setMessage("") } }}>
               <AddIcon fontSize="inherit" />
             </IconButton>
           </Paper>
         </div>
+
         <List className={styles.toDoList}>
           {messages.map((message) => (
             <ListItem className={styles.listElement}>
