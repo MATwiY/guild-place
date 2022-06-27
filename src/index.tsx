8a72da17d6fb0da4101a1da8d25fd9140a9e4d0c
Mateusz <mateuszwidalski@gmail.com>
2022-06-27 17:17:38 +0200

    f: add requested changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import { DashboardPage } from "./modules/Dashboard/pages";
 import { WelcomePage } from "./modules/Welcome/pages/WelcomePage";
 
 const root = ReactDOM.createRoot(
@@ -11,11 +10,14 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <WelcomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
